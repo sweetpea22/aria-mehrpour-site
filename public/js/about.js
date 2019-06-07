681ab68b713b6f9017c0e4e872a67f1bb962300f
Cindy Sweetpea <cindychau24@me.com>
2019-06-07 11:07:21 -0400

    change about page content

--- public/js/about.js
+++ public/js/about.js
@@ -1,11 +1,24 @@
+const domStrings = {
+  menuBtn: '.nav__collapse',
+  navMenu: '.nav__container',
+  allProjects: '#projectsLink',
+  subNav: '.sub__nav'
+}
+
+
+const menuBtn = document.querySelector(domStrings.menuBtn),
+navMenu = document.querySelector(domStrings.navMenu),
+allProjects = document.querySelector(domStrings.allProjects),
+subNav = document.querySelector(domStrings.subNav)
+
 // *** display/hide menu *** //
 
 menuBtn.addEventListener('click', e => {
   navMenu.classList.toggle('is-open');
   if(navMenu.className.includes('is-open')){
-    e.target.style.color = 'black';
+    e.target.style.color = 'yellow';
     menuBtn.innerHTML = '<strong>CLOSE<strong>'
-    menuBtn.style.left = '5.5%';
+    // menuBtn.style.left = '5.5%';
   } else {
     menuBtn.innerHTML = 'MENU';
   }
@@ -14,6 +27,9 @@ menuBtn.addEventListener('click', e => {
 allProjects.addEventListener('mouseenter', e => {
   subNav.classList.add('is-open');
 })
+// .addEventListener('click', e => {
+//   subNav.classList.add('is-open')
+// })
 
 subNav.addEventListener('mouseenter', e => {
   allProjects.classList.toggle('is-selected');
