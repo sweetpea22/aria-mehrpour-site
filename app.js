e62cd7d7365e917b19b4b4c16264cbf3c97cba2f
Cindy Sweetpea <cindychau24@me.com>
2019-06-07 13:17:44 -0400

    Updates

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
-      app = express();
+      app = express(),
+      path = require('path');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
 
-
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
   res.render('landing');
 });
 
