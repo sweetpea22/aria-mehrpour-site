3fc8825a69e513347940bac24695823cbb1414ee
Cindy Sweetpea <cindychau24@me.com>
2019-06-07 16:22:20 -0400

    add index.html

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
-      app = express();
+      app = express(),
+      path = require('path');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
-
 
 
 app.get('/', (req, res) => {
