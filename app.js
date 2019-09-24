1ea9eab8bbfbc9363257adf8ad0b6f877b1cb1b7
Cindy Sweetpea <cindychau24@me.com>
2019-09-24 12:32:18 -0400

    fixed post route duplicate

--- app.js
+++ app.js
@@ -17,7 +17,48 @@ app.post('/contact', urlencodedParser, (req, res) => {
   if (req.body.contact__body === ' ' || req.body.contact__email === '' || req.body === null) {
     res.send('Invalid form input.');
   } else {
-    res.render('contact-success', { data: req.body });
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        type: "OAuth2",
+        user: process.env.GMAIL_USER,
+        clientId: process.env.GMAIL_CLIENTID,
+        clientSecret: process.env.GMAIL_CLIENTSECRET,
+        refreshToken: process.env.GMAIL_REFRESHTOKEN,
+        accessToken: process.env.GMAIL_ACCESSTOKEN
+      }
+    })
+
+    let mailOpts = {
+      from: "Dolo.world Form nocturne8998@gmail.com",
+      to: "Aria Mehrpour Site nocturne8998@gmail.com",
+      subject: "Contact Request via Website Form",
+      html: `
+      <p> You have a new contact request</p> 
+      <h3>Contact Details</h3>
+      <ul>
+      <li> Name: ${req.body.contact__name}</li>
+      <li> Email: ${req.body.contact__email}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.contact__body}</p>
+      `
+    }
+    transporter.sendMail(mailOpts, (err, info) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('contact-success', { data: req.body });
+        console.log("info.messageId: " + info.messageId);
+        console.log("info.accepted: " + info.accepted);
+        console.log("info.rejected: " + info.rejected);
+        console.log("info.pending: " + info.pending);
+        console.log("info.response: " + info.response);
+      }
+      transporter.close();
+    });
   }
 });
 
@@ -43,46 +84,6 @@ app.get('/portraits', (req, res) => {
 
 app.get('/branding-shoots', (req, res) => {
   res.render('more/branding');
-});
-
-app.post('/contact', (req, res) => {
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      type: "OAuth2",
-      user: process.env.GMAIL_USER,
-      clientId: process.env.GMAIL_CLIENTID,
-      clientSecret: process.env.GMAIL_CLIENTSECRET,
-      refreshToken: process.env.GMAIL_REFRESHTOKEN,
-      accessToken: process.env.GMAIL_ACCESSTOKEN
-    }
-  })
-
-  let mailOpts = {
-    from: "Dolo.world Form nocturne8998@gmail.com",
-    to: "Aria Mehrpour Site nocturne8998@gmail.com",
-    subject: "Contact Request via Website Form",
-    text: `${req.body.contact__name} would like to get in touch with you. The email address they left is ${req.body.contact__email}. 
-    Message: ${req.body.contact__body}
-    `,
-    html: "hello"
-  }
-  transporter.sendMail(mailOpts, (err, info) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect('contact-success');
-      console.log("info.messageId: " + info.messageId);
-      console.log("info.envelope: " + info.envelope);
-      console.log("info.accepted: " + info.accepted);
-      console.log("info.rejected: " + info.rejected);
-      console.log("info.pending: " + info.pending);
-      console.log("info.response: " + info.response);
-    }
-    transporter.close();
-  });
 });
 
 const port = process.env.PORT || 3000;
